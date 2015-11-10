78587d07a50a351cce73b6589ec19b830a509194
Colin Hoglund <colinhoglund@gmail.com>
2015-11-10 01:20:12 -0500

    _add_tag_filters helper function, add get_snapshots

--- fabboto/aws/ec2.py
+++ fabboto/aws/ec2.py
@@ -2,16 +2,7 @@
 import boto3
 
 def get_instances(state=None, tags=None):
-    """
-    Get ec2 instances based on filters.
-
-    Parameters
-        state: string
-        tags: dictionary
-            ex: {'Name': 'web2', 'InVPC': 'True'}
-
-    Returns a boto3 collection
-    """
+    """ Get ec2 instances based on filters. """
     ec2 = boto3.resource('ec2')
     ec2_filter = []
 
@@ -19,17 +10,27 @@ def get_instances(state=None, tags=None):
     if state:
         ec2_filter.append({'Name': 'instance-state-name', 'Values': [state]})
     if tags:
-        for tag in tags.iteritems():
-            ec2_filter.append({'Name': 'tag:{0}'.format(tag[0]), 'Values': [tag[1]]})
+        _add_tag_filters(tags, ec2_filter)
 
     #return a collection of ec2 instances
     return ec2.instances.filter(Filters=ec2_filter)
 
-def get_snapshots(owner_id):
-    """
-    Get ec2 snapshots based on filters
-    """
+def get_snapshots(owner_id, status=None, tags=None):
+    """ Get ec2 snapshots based on filters """
     ec2 = boto3.resource('ec2')
     ec2_filter = []
 
+    # apply filters based on arguments
     ec2_filter.append({'Name': 'owner-id', 'Values': [owner_id]})
+
+    if status:
+        ec2_filter.append({'Name': 'status', 'Values': [status]})
+    if tags:
+        _add_tag_filters(tags, ec2_filter)
+
+    #return a collection of ec2 snapshots
+    return ec2.snapshots.filter(Filters=ec2_filter)
+
+def _add_tag_filters(tags, ec2_filter):
+   for tag in tags.iteritems():
+       ec2_filter.append({'Name': 'tag:{0}'.format(tag[0]), 'Values': [tag[1]]})
