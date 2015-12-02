258959d0b121ba3fce025adb4e3327f1864e122d
Colin Hoglund <colinhoglund@gmail.com>
2015-12-02 10:38:13 -0500

    adding module for route53

---
+++ aws/route53.py
@@ -0,0 +1,19 @@
+""" Functions for interacting with AWS Route53 """
+import boto3
+
+CONN = boto3.client('route53')
+
+def get_record_sets(zone):
+    """ Get a list of record sets for a specified zone """
+
+    # add trailing dot
+    if zone[-1] != '.':
+        zone += '.'
+
+    # use zone paginator to determine HostedZoneId
+    zone_iter = CONN.get_paginator('list_hosted_zones').paginate()
+    zone = list(zone_iter.search("HostedZones[?Name == '{}'].Id".format(zone)))[0]
+
+    # use record paginator to return list of RecordSets
+    rec_paginator = CONN.get_paginator('list_resource_record_sets')
+    return list(rec_paginator.paginate(HostedZoneId=zone).search('ResourceRecordSets[].Name'))
