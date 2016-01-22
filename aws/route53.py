e15b1bee771050f9e0624c2d7b5153cce6289e7f
Colin Hoglund <colinhoglund@gmail.com>
2016-01-22 02:10:54 -0500

    updating route53 to use jmespath filters

--- aws/route53.py
+++ aws/route53.py
@@ -1,68 +1,51 @@
 ''' Functions for interacting with AWS Route53 '''
 import boto3
-from aws import ec2
+from aws import ec2, jmespath
 
 CONN = boto3.client('route53')
 
 def get_zones(domains=None):
     'return hosted zones'
 
+    # get zones for specified domains
+    jmes_filter = jmespath.FilterProjection()
+    if domains:
+        # convert string arg to list and normalize dns name with a .
+        domains = _str_to_list(domains)
+        domains = ['{}.'.format(x) if x[-1] != '.' else x for x in domains]
+
+        jmes_filter.add_aggregate('Name', domains)
+
     # use zone paginator to gather all zones
     zone_iter = CONN.get_paginator('list_hosted_zones').paginate()
-    zones = list(zone_iter)[0]
+    return list(zone_iter.search("HostedZones[{}]".format(jmes_filter)))
 
-    # get zones for specified domains
-    if domains:
-        # cast domains to list to accept str or list
-        if isinstance(domains, str):
-            domains = domains.split()
+def get_zone_ids(domains=None):
+    'return a dict of {domain: zone_id}'
+    return [zone['Id'] for zone in get_zones(domains)]
 
-        zone_list = []
-        for domain in domains:
-            # append missing period
-            if domain[-1] != '.':
-                domain += '.'
-
-            # append domain to zones
-            zone_list += list(zone_iter.search("HostedZones[?Name == '{}']".format(domain)))
-        zones['HostedZones'] = zone_list
-    return zones
-
-def get_records(domains=None, record_types=None):
+def get_records(names=None, domains=None, types=None):
     'return records'
 
-    # get a list of zone ids based on value of domains
-    zone_ids = [zone['Id'] for zone in get_zones(domains)['HostedZones']]
+    jmes_filter = jmespath.FilterProjection()
+    if names:
+        jmes_filter.add_aggregate('Name', names)
+    if types:
+        types = _str_to_list(types)
+        jmes_filter.add_filter('Type', types)
 
-    # use record paginator to initialize return value
-    rec_iter = CONN.get_paginator('list_resource_record_sets').paginate(HostedZoneId=zone_ids[0])
-    records = list(rec_iter)[0]
+    # get a list of zone ids based on value of domains
+    zone_ids = get_zone_ids(domains)
 
     # loop through zone_ids to gather records
-    rec_list = []
+    records = []
     for zone_id in zone_ids:
         # use paginator for each zone
         rec_iter = CONN.get_paginator('list_resource_record_sets').paginate(HostedZoneId=zone_id)
-
-        # search only for specified record types
-        if record_types:
-            # cast records_types to list to accept str or list
-            if isinstance(record_types, str):
-                record_types = record_types.split()
-
-            # search for specified record types and add them to rec_list
-            for rec_type in record_types:
-                rec_search = rec_iter.search("ResourceRecordSets[?Type == '{}']".format(rec_type))
-                rec_list += list(rec_search)
-        # add all records in zone to rec_list
-        else:
-            rec_list += list(rec_iter)[0]['ResourceRecordSets']
-
-    # update records and return
-    records['ResourceRecordSets'] = rec_list
+        records += list(rec_iter.search("ResourceRecordSets[{}]".format(jmes_filter)))
     return records
 
-def get_unused_records(domains=None, record_types=None):
+def get_unused_records(domains=None, types=None):
     'returns all records currently unused by ec2 instances'
     # this currently only checks running instances
     #instances = ec2.get_instances()
@@ -78,7 +61,7 @@ def get_unused_records(domains=None, record_types=None):
     # this needs to be refactored to do checks after the list of
     # unused_records is complete. Likely in the following order
     # A -> CNAME -> Alias -> CNAME again
-    for record in get_records(domains, record_types)['ResourceRecordSets']:
+    for record in get_records(domains, types)['ResourceRecordSets']:
         if record.has_key('ResourceRecords'):
             if record['Type'] == 'A':
                 if record['ResourceRecords'][0]['Value'] not in ips:
@@ -92,3 +75,27 @@ def get_unused_records(domains=None, record_types=None):
                 unused_records.append(record['Name'])
 
     return unused_records
+
+def delete_records(names):
+    'create hosted zone'
+
+    names = _str_to_list(names)
+
+    # build dictionary of records using zone_id as the key
+    records = {}
+    for name in names:
+        domain = '.'.join(name.split('.')[-2:])
+        if not records.has_key(domain):
+            records[domain] = []
+        records[domain].append(name)
+
+    for domain in records.items():
+        print domain
+        print get_records(names=domain[1])
+
+    return None
+
+def _str_to_list(obj):
+    if isinstance(obj, str):
+        return obj.split()
+    return obj
